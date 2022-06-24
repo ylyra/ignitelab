8eab0f300dd48320674ed8d46380dbf48c4cada0
Yan Lyra <yanlyra3@gmail.com>
2022-06-23 21:04:35 -0300

    feat(component): created html elements for Video component

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,113 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Image,
+  Lightning,
+} from "phosphor-react";
+import "@vime/core/themes/default.css";
+
 export function Video() {
   return (
-    <div className="flex-1" />
-  )
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="KJj70dBgRPo" />
+            <DefaultUi />
+          </Player>
+        </div>
+      </div>
+
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">Aula 01</h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              Chegamos na metade do nosso evento, mas ainda tem mais pela
+              frente…Na terceira aula vamos continuar nosso projeto,
+              desenvolvendo o roteamento e player. Essa é mais uma etapa para
+              sua especialização em React!
+            </p>
+
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                src="https://github.com/ylyra.png"
+                alt="git user photo"
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+              />
+
+              <div className="leading-relaxed">
+                <strong className="text-2xl block">Yan Lyra</strong>
+                <span className="text-gray-200 text-sm block">
+                  FullStack @Coopers
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <a
+              href=""
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Comunidade do Discord
+            </a>
+
+            <a
+              href=""
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
+              <Lightning size={24} />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-8 mt-20">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Material complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelear o seu
+                desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <Image size={40} />
+            </div>
+
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers exclusivos</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe wallpapers exclusivos da Maratona Explorer e personalize a
+                sua máquina
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
